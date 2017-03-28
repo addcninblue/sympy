0de5517037da5a4d8a3c5dd0199ebfcb7e3f7232
Add Chan <addison95132@gmail.com>
2017-03-27 17:41:49 -0700

    added number 20

--- purplecomet2009/num20.py
+++ purplecomet2009/num20.py
@@ -1,4 +1,35 @@
 #!/bin/env python
 from itertools import permutations
-allperms = sorted(''.join(chars) for chars in set(permutations('WWWWWWWMMMMM')))
-print(allperms)
+from sympy import *
+
+
+def isWoman(i):
+    for key, c in enumerate(i):
+        if c == 'M':
+            if key == 0:
+                if i[1] != 'W':
+                    return False
+            elif key == len(i) - 1:
+                if i[len(i) - 2] != 'W':
+                    return False
+            else:
+                if i[key - 1] != 'W' and i[key + 1] != 'W':
+                    return False
+    return True
+
+
+def main():
+    allperms = sorted(''.join(chars) for chars in set(permutations('WWWWWWWMMMMM')))
+    print(len(allperms))
+
+    count = 0
+    for i in allperms:
+        if isWoman(i):
+            count += 1
+    print(count)
+    print(f'{count}/{len(allperms)}')
+    ans = (len(allperms) + count) / (gcd(len(allperms), count))
+    print(ans) # 287
+
+
+main()
